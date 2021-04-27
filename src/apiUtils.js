96e49db4fb1562c63e1236988fe4db76a04550b6
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-04-26 20:06:36 -0400

    made auth routes, displaying on FE, connected with BE

--- src/apiUtils.js
+++ src/apiUtils.js
@@ -1,22 +1,39 @@
 import request from 'superagent';
 
-
-
 const localURL = 'http://localhost:7890';
 
 export async function getTest() {
-    const response = await request
-    .get(`${localURL}/test`)
-    return response.body
+  const response = await request.get(`${localURL}/test`);
+  return response.body;
 }
 
 export async function postImage(photo) {
-    const response = await request
+  const response = await request
     .post(`${localURL}/imageurl`)
-    .send([{input: photo}])
+    .send([{ input: photo }]);
 
-    console.log(response.body, 'API')
+  console.log(response.body, 'API');
 
-    return response.body
+  return response.body;
 }
 
+export async function postAuthImage(photo) {
+  const response = await request
+    .post(`${localURL}/api/v1/auth/imageurl`)
+    .send([{ input: photo }]);
+
+  console.log(response.body, 'API');
+
+  return response.body;
+}
+
+export async function login(email, password) {
+  console.log(email, password, 'email,pass');
+  const response = await request
+    .post(`${localURL}/api/v1/auth/login`)
+    .send({ email, password });
+
+  console.log(response.body, 'This is the login function');
+
+  return response.body;
+}
