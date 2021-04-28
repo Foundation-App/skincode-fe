a87b1559c2f01913e31e61b3c415ef3347b88738
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-04-28 18:26:53 -0400

    displaying two arrays with best and good results

--- src/components/imageForm/Cloudinary.js
+++ src/components/imageForm/Cloudinary.js
@@ -1,101 +1,112 @@
-import React, { Component } from 'react'
-import { postImage } from '../../apiUtils'
+import React, { Component } from 'react';
+import { postImage } from '../../apiUtils';
 import FoundationList from './FoundationList';
 import NavBar from '../HomePage/NavBar';
 import Icon3 from '../../images/logo3.gif';
 import Icon2 from '../../images/logo2.gif';
 import Icon1 from '../../images/logo1.gif';
 import {
-    StepsContainer,
-    StepsH1,
-    StepsWrapper,
-    StepsCard,
-    StepsIcon,
-    StepsH2,
-    StepsP
-  } from './StepsStyling';
+  StepsContainer,
+  StepsH1,
+  StepsWrapper,
+  StepsCard,
+  StepsIcon,
+  StepsH2,
+  StepsP
+} from './StepsStyling';
 
 export default class Cloudinary extends Component {
+  state = {
+    // foundations: [],
+    bestFoundations: [],
+    goodFoundations: [],
+    loading: true
+  };
 
-    state = { 
-        foundations: [],
-        loading: true
-    }
-
-    showWidget = () => {
-        this.setState({foundations: [] });
-        let widget = window.cloudinary.createUploadWidget({ 
-           cloudName: `skincode`,
-           uploadPreset: `test_preset`, 
-           cropping: true,
-           sources: ['local', 'url', 'camera'],
-           styles : { 
-             palette: { 
-                window: "#FFF",
-                windowBorder: "#90A0B3",
-                tabIcon: "#0E2F5A",
-                menuIcons: "#5A616A",
-                textDark: "#000000",
-                textLight: "#FFFFFF",
-                link:  "#0078FF",
-                action:  "#FF620C",
-                inactiveTabIcon: "#0E2F5A",
-                error: "#F44235",
-                inProgress: "#0078FF",
-                complete: "#20B832",
-                sourceBg: "#FFF"
-                 
-             }  
-           }
-        },
-        (error, result) => {
-          if (!error && result && result.event === "success") { 
-              console.log(result.info.url, 'here is your new link!'); 
-              postImage(result.info.url).then(makeupData => this.setState({
-                  foundations: makeupData
-              }));
-        } 
-  
-        });
-        widget.open()
+  showWidget = () => {
+    this.setState({ foundations: [] });
+    let widget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: `skincode`,
+        uploadPreset: `test_preset`,
+        cropping: true,
+        sources: ['local', 'url', 'camera'],
+        styles: {
+          palette: {
+            window: '#FFF',
+            windowBorder: '#90A0B3',
+            tabIcon: '#0E2F5A',
+            menuIcons: '#5A616A',
+            textDark: '#000000',
+            textLight: '#FFFFFF',
+            link: '#0078FF',
+            action: '#FF620C',
+            inactiveTabIcon: '#0E2F5A',
+            error: '#F44235',
+            inProgress: '#0078FF',
+            complete: '#20B832',
+            sourceBg: '#FFF'
+          }
+        }
+      },
+      (error, result) => {
+        if (!error && result && result.event === 'success') {
+          console.log(result.info.url, 'here is your new link!');
+          postImage(result.info.url).then((makeupData) =>
+            this.setState({
+              // foundations: makeupData,
+              bestFoundations: makeupData[0],
+              goodFoundations: makeupData[1]
+            })
+          );
+        }
       }
+    );
+    widget.open();
+  };
 
-
-    render() {
-        return (
-            <div>
-            <NavBar></NavBar>
-            <StepsContainer id='Steps'>
-            <StepsH1>How it Works.</StepsH1>
-            <StepsWrapper>
-              <StepsCard>
+  render() {
+    return (
+      <div>
+        <NavBar></NavBar>
+        <StepsContainer id="Steps">
+          <StepsH1>How it Works.</StepsH1>
+          <StepsWrapper>
+            <StepsCard>
               <StepsIcon src={Icon1} />
-                <StepsH2>We wanna see your selfies!</StepsH2>
-                <StepsP>
-                  We want to see that beautiful face. Use our image uploader to either take a selfie, upload a selfie, or paste in a URL image. Just make sure you're in natural lighting for accurate results.
-                </StepsP>
-              </StepsCard>
-              <StepsCard>
+              <StepsH2>We wanna see your selfies!</StepsH2>
+              <StepsP>
+                We want to see that beautiful face. Use our image uploader to
+                either take a selfie, upload a selfie, or paste in a URL image.
+                Just make sure you're in natural lighting for accurate results.
+              </StepsP>
+            </StepsCard>
+            <StepsCard>
               <StepsIcon src={Icon2} />
-                <StepsH2>Upload Image</StepsH2>
-                <StepsP>
-                  Once you feel like you've got the best picture. Go ahead and hit upload and watch the magic unfold.
-                </StepsP>
-              </StepsCard>
-              <StepsCard>
+              <StepsH2>Upload Image</StepsH2>
+              <StepsP>
+                Once you feel like you've got the best picture. Go ahead and hit
+                upload and watch the magic unfold.
+              </StepsP>
+            </StepsCard>
+            <StepsCard>
               <StepsIcon src={Icon3} />
-                <StepsH2>Unlock your skincode</StepsH2>
-                <StepsP>
-                  You've got a match! Feel free to favorite your best matches. If there aren't any matches, start again and make sure you're following all the steps.
-                </StepsP>
-              </StepsCard>
-            </StepsWrapper>
-            <button className="upload" onClick={this.showWidget}>Upload Image</button>
-            <FoundationList
-                    mapFoundations={this.state.foundations}/>
-          </StepsContainer>
-          
-            {/* // <div className="instructions">
+              <StepsH2>Unlock your skincode</StepsH2>
+              <StepsP>
+                You've got a match! Feel free to favorite your best matches. If
+                there aren't any matches, start again and make sure you're
+                following all the steps.
+              </StepsP>
+            </StepsCard>
+          </StepsWrapper>
+          <button className="upload" onClick={this.showWidget}>
+            Upload Image
+          </button>
+          <FoundationList mapFoundations={this.state.bestFoundations} />
+          <FoundationList mapFoundations={this.state.goodFoundations} />
+        </StepsContainer>
+
+        {/* // <div className="instructions">
             //      <div className="title">Are you ready to find your SkinCode()?</div>
             //     <ul className="steps">
             //         <li>Take a Selfie</li>
@@ -104,10 +115,7 @@ export default class Cloudinary extends Component {
             //         <li>Upload your picture</li>
             //         <li>Let us do the rest!!!</li>
             //     </ul> */}
-              
-                
-               
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
--- src/components/imageForm/FoundationList.js
+++ src/components/imageForm/FoundationList.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
-import Foundation from './Foundation'
+import React, { Component } from 'react';
+import Foundation from './Foundation';
 
 export default class FoundationList extends Component {
-    render() {
-        return (
-            <div className="foundation-big-box">
-                <ul className="foundation">
-                    {this.props.mapFoundations.map(oneFoundation => <Foundation
-                        key={oneFoundation.hex}
-                        foundationProp={oneFoundation}/>)}
-                </ul>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="foundation-big-box">
+        <ul className="foundation">
+          {this.props.mapFoundations.map((oneFoundation) => (
+            <Foundation
+              key={oneFoundation.hex}
+              foundationProp={oneFoundation}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
