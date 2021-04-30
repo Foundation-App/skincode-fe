c6fa658f35b41a03f2554adaf68107ce3eab8220
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-04-29 20:20:02 -0400

    attempt to deploy to netlify

--- src/apiUtils.js
+++ src/apiUtils.js
@@ -1,6 +1,7 @@
 import request from 'superagent';
 
-const localURL = 'http://localhost:7894';
+// const localURL = 'http://localhost:7894';
+const localURL = 'https://skincodeprod.herokuapp.com';
 
 export async function getTest() {
   const response = await request.get(`${localURL}/test`);
