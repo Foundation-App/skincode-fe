9763351fac6cea7cf5257bfbe040db082a3dc909
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-04-29 21:55:22 -0400

    please deploy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,103 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
-import logo from '../src/skincode-logo.gif'
 import Cloudinary from './components/imageForm/Cloudinary';
-// import {Imageform} from './components/imageForm/Imageform';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/auth/login';
+import Logout from './components/auth/logout';
+import Signup from './components/auth/signup';
+import FavoritesPage from './components/favorites/FavoritesPage';
+import HomePage from './components/HomePage/HomePage';
+import PrivateRoute from './components/auth/PrivateRoute';
+import { putUserInLocalStorage, verifyUser } from './apiUtils';
 
 export default class App extends Component {
+  state = {
+    user: null,
+    authError: '',
+    authLoading: true
+  };
+
+  handleUserChange = (user) => {
+    this.setState({ user });
+
+    putUserInLocalStorage(user);
+  };
+
+  handleLogout = () => {
+    this.handleUserChange();
+    sessionStorage.clear();
+    // saveCookies.clear
+  };
+
+  componentDidMount = () => {
+    this.setState({ authLoading: true });
+    verifyUser()
+      .then((user) =>
+        this.setState({
+          user,
+          loading: false
+        })
+      )
+      .catch((authError) => {
+        this.setState({ authError: authError.message });
+        console.log(authError.message);
+      });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header><img className="logo" src={logo} alt="logo"/></header>
-        < Cloudinary/>
-        
+      <div>
+        <Router>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={(routerProps) => <HomePage {...routerProps} />}
+            />
+            <Route
+              path="/signup"
+              exact
+              render={(routerProps) => <Signup {...routerProps} />}
+            />
+            <Route
+              path="/login"
+              exact
+              render={(routerProps) => <Login {...routerProps} />}
+            />
+            <Route
+              path="/findmyskincode"
+              exact
+              render={(routerProps) => <Cloudinary {...routerProps} />}
+            />
+            <Route
+              path="/logout"
+              exact
+              render={(routerProps) => <Logout {...routerProps} />}
+            />
+            <PrivateRoute
+              exact
+              path="/myfavorites"
+              render={(routerProps) => <FavoritesPage {...routerProps} />}
+              user={this.state.user}
+              error={this.state.authError}
+              loading={this.state.authLoading}
+            />
+            {/* <Route
+              path="/myfavorites"
+              exact
+              render={(routerProps) => <FavoritesPage {...routerProps} />}
+            /> */}
+          </Switch>
+        </Router>
       </div>
-    )
+    );
   }
 }
 
-
+// <PrivateRoute
+// path="/playlist"
+// exact
+// token={user && user.token}
+// render={(routerProps) => <Playlist {...routerProps} />}
+// user={this.state.user}
+// />
